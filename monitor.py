ce39788057b81c7516cc0a34855fbd04331aff59
kveca1 <kveca1@vols.utk.edu>
2017-02-25 03:54:41 -0500

    added monitor.py to check for new files and call dataProcessing

---
+++ monitor.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+
+folder = "."
+command_to_run = "ls -t | head -n 1"
+finalcommand = "./hellofile "
+
+def get_drlist():
+    return subprocess.check_output(["ls", folder]).decode('utf-8').strip().split("\n")
+
+while True:
+    drlist1 = get_drlist()
+    time.sleep(2)
+    drlist2 = get_drlist()
+    if len(drlist2) > len(drlist1):
+       p = subprocess.check_output(["/bin/bash", "-c", command_to_run])
+       finalcommand += p
+       subprocess.Popen(["/bin/bash", "-c", finalcommand])
+       
